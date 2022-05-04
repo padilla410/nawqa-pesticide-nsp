57df535665c362d56cc2f24292dcd34bbc67a64a
Julie Padilla <padilla410@gmail.com>
2022-05-04 10:53:22 -0700

    repo reorg and tracking input files

--- 1_fetch.R
+++ 1_fetch.R
@@ -1,43 +1,40 @@
 source('1_fetch/src/fetch_pesticide_data.R')
 
-# fetch pesticide data:
-## fetch all pesticide dbfs in file
-## fetch US raster in file
-## fetch all descriptive data in file (bins and labels)
-## fetch crosswalk of pesticide file names and 
-## create a vector of pesticide names for late munging
-
 p1_targets_list <- list(
   tar_target(
-    #load files
-    p1_pest_dbf
-    
+    p1_pest_of_interest,
+    generate_pest_vector(file_path = '1_fetch/in/maps/dbf19')
   ),
   
+  # Track changes in high estimate dbf files
   tar_target(
-    # load US raster
-    p1_us_raster
+    p1_pest_hi_dbf,
+    list_pest_dbf(file_path = '1_fetch/in/maps/dbf19', 
+                  est_type = 'High'),
+    format = 'file'
   ),
   
+  # Track changes in low estimate dbf files
   tar_target(
-    # create vector of pesticide names (`pest_list`) from loaded files (previous target)
-    # to filter the next 3 targets
-    p1_pest_list
+    p1_pest_lo_dbf,
+    list_pest_dbf(file_path = '1_fetch/in/maps/dbf19', 
+                  est_type = 'Low'),
+    format = 'file'
   ),
   
+  # Track changes in pesticide bins for pesticides of interest
   tar_target(
-    # load crosswalk of pesticide names (`pesticide_names.txt`)
-    p1_pest_name_xwalk
+    p1_pest_bin_csv,
+    list_pest_csv(file_path = '1_fetch/in/maps/bins', 
+                  poi = p1_pest_of_interest),
+    format = 'file'
   ),
   
+  # Track changes in pesticide labels for pesticides of interest
   tar_target(
-    # use pest_list to load pesticide percentile bins of interest
-    p1_pest_bins
-  ),
-  
-  tar_target(
-    # use pest_list to load pesticide legend lables of interest
-    p1_pest_labels
+    p1_pest_label_csv,
+    list_pest_csv(file_path = '1_fetch/in/maps/labels', 
+                  poi = p1_pest_of_interest),
+    format = 'file'
   )
-
 )
--- _targets.R
+++ _targets.R
@@ -20,48 +20,15 @@ library(maps)
 library(glue)
 library(sf)
 
-source('src/web_map_functions.R')
-# source('1_fetch/src/fetch_pesticide_data.R')
+# setting options
+options(tidyverse.quiet = TRUE)
+
+source('1_fetch.R')
 # source('2_process.R')
 # source('3_visualize.R')
 
-# source('draft_docs/fetch_pesticide_data.R')
+# tar_option_set(debug = "p1_pest_bin_csv")
+# year <- '2019'
 
-year <- '2019'
+c(p1_targets_list)
 
-
-
-# tar_target(
-#   pest_vector,
-#   generate_pest_vector(file_path = '1_fetch/in/maps/dbf19')
-# )
-# # Return the complete list of targets
-# # c(p1_targets_list, p2_targets_list, p3_targets_list)
-
-
-
-year <- '2019'
-
-# load plot variables
-list(
-  tar_target(
-    pesticides_of_interest,
-    generate_pest_vector(file_path = '1_fetch/in/maps/dbf19')
-    ),
-
-  tar_target(
-    pesticide_labels,
-    read_pest_files(pesticides_of_interest,
-                    file_path = '1_fetch/in/maps/labels', col_types = rep('c', 4)),
-    pattern = map(pesticides_of_interest)
-  ),
-
-  tar_target(
-    pesticide_bins,
-    read_pest_files(pesticides_of_interest,
-                    file_path = '1_fetch/in/maps/bins', col_types = rep('d', 5)),
-    pattern = map(pesticides_of_interest)
-  )
-)
-
-# # make plots
